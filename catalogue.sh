1d4c26093e49f11ae44264b2a43e61c1b4d678ba
anithaaluru <info@devops.com>
2025-10-31 13:35:48 +0530

    roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -1,71 +1,9 @@
-#!/bin/bash
-USER_ID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop.log"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-mkdir -p $LOGS_FOLDER
-echo -e "$R this script executed at: $(date) $N" | tee -a $LOG_FILE
-SCRIPT_DIR=$PWD
-
-if [ $USER_ID -ne 0 ]
- then
-   echo -e "$R ERROR::please run this script using root access $N" | tee -a $LOG_FILE
- else
-   echo -e "$Y you are running this script with root access $N" | tee -a $LOG_FILE
-fi
-
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-     echo -e "$2 is....$G success $N" | tee -a $LOG_FILE
-    else
-     echo -e "$2 is....$R failure $N" | tee -a $LOG_FILE
-    fi
-}
-
-dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "disabling nodejs"
-
-dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? "enabling nodejs"
-
-dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "installing nodejs"
-
- id roboshop
- if [ $? -ne 0 ]
- then
-   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
-   VALIDATE $? "creating roboshop system user"
- else
-   echo -e "$G user is already created $N" 
- fi
-
-mkdir -p /app 
-VALIDATE $? "creating app directory"
-
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
-VALIDATE $? "downloading catalogue"
-
-rm -rf /app/*
-cd /app 
-unzip /tmp/catalogue.zip &>>$LOG_FILE
-VALIDATE $? "unzipping the catalogue"
-
-npm install &>>$LOG_FILE
-VALIDATE $? "installing dependencies"
-
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
-VALIDATE $? "copying the catalogue service"
-
-systemctl daemon-reload &>>$LOG_FILE
-systemctl enable catalogue  &>>$LOG_FILE
-systemctl start catalogue &>>$LOG_FILE
-VALIDATE $? "starting the catalogue"
+source ./common.sh
+app_name=catalogue
+check_root
+nodejs_setup
+app_setup
+systemd_setup
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "copying the mongodb repo"
--- common.sh
+++ common.sh
@@ -10,27 +10,65 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 mkdir -p $LOGS_FOLDER
 echo "script executed at: $(date)" | tee -a $LOG_FILE
+SCRIPT_DIR=$PWD
 
 check_root(){
-if [ $USER_ID -ne 0 ]
- then
-  echo -e "$R ERROR::please run this script with root  access $N" | tee -a $LOG_FILE
-  exit 1
- else
-  echo -e "$Y you are running this script with root access $N" | tee -a $LOG_FILE
-fi
+    if [ $USER_ID -ne 0 ]
+     then
+       echo -e "$R ERROR::please run this script with root  access $N" | tee -a $LOG_FILE
+       exit 1
+    else
+       echo -e "$Y you are running this script with root access $N" | tee -a $LOG_FILE
+    fi
 }
-  VALIDATE(){
+   VALIDATE(){
    if [ $1 -ne 0 ]
     then
      echo -e "$2 is...... $R failure $N" | tee -a $LOG_FILE
    else
      echo -e "$2 is.... $G success $N" | tee -a $LOG_FILE
    fi
-  }
-  print_time(){
-  END_TIME=$(date +%s)
-  TOTAL_TIME=$(($END_TIME - $START_TIME))
-  echo "script executed time:$TOTAL_TIME seconds"
-  }
+   }
+nodejs_setup(){
+    dnf module disable nodejs -y &>>$LOG_FILE
+    VALIDATE $? "disabling nodejs"
+    dnf module enable nodejs:20 -y &>>$LOG_FILE
+    VALIDATE $? "enabling nodejs"
+    dnf install nodejs -y &>>$LOG_FILE
+    VALIDATE $? "installing nodejs"
+}
+app_setup(){
+    id roboshop
+      if [ $? -ne 0 ]
+        then
+         useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+         VALIDATE $? "creating roboshop system user"
+      else
+         echo -e "$G user is already created $N" 
+      fi
+      mkdir -p /app 
+      VALIDATE $? "creating app directory"
+}
+systemd_setup(){   
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOG_FILE
+    VALIDATE $? "downloading $app_name"
+    rm -rf /app/*
+    cd /app 
+    unzip /tmp/$app_name.zip &>>$LOG_FILE
+    VALIDATE $? "unzipping the $app_name"
+    npm install &>>$LOG_FILE
+    VALIDATE $? "installing dependencies"
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
+    VALIDATE $? "copying the $app_name service"
+    systemctl daemon-reload &>>$LOG_FILE
+    systemctl enable $app_name  &>>$LOG_FILE
+    systemctl start $app_name &>>$LOG_FILE
+    VALIDATE $? "starting the $app_name"
+}
+
+print_time(){
+    END_TIME=$(date +%s)
+    TOTAL_TIME=$(($END_TIME - $START_TIME))
+    echo "script executed time:$TOTAL_TIME seconds"
+}
   
