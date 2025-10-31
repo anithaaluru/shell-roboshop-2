ed06fede553293e29f584a4fdc5582aac5a7c903
anithaaluru <info@devops.com>
2025-10-31 20:32:06 +0530

    roboshop

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,6 +1,8 @@
 
  source ./common.sh
  app_name=rabbitmq
+ echo "please enter rabbitmq password:"
+ read -s "RABBITMQ_PASSWD"
  cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
   VALIDATE $? "copying rabbitmq repo"
   dnf install rabbitmq-server -y &>>LOG_FILE
