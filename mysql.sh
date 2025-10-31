4722cb98afcec008ba04c616b45d9c52ad010941
anithaaluru <info@devops.com>
2025-10-31 19:48:22 +0530

    roboshop

--- mysql.sh
+++ mysql.sh
@@ -1,38 +1,13 @@
 
-#!/bin/bash
-USER_ID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-mkdir -p $LOGS_FOLDER
-echo "script executed at: $(date)" | tee -a $LOG_FILE
-
-if [ $USER_ID -ne 0 ]
- then
-  echo -e "$R ERROR::please run this script with root  access $N" | tee -a $LOG_FILE
-  exit 1
- else
-  echo -e "$Y you are running this script with root access $N" | tee -a $LOG_FILE
-fi
-  echo "please enter the root password"
-  read -s "MYSQL_ROOT_PASSWORD"
-  
-  VALIDATE(){
-   if [ $1 -ne 0 ]
-    then
-     echo -e "$2 is...... $R failure $N" | tee -a $LOG_FILE
-   else
-     echo -e "$2 is.... $G success $N" | tee -a $LOG_FILE
-   fi
-  }
-  dnf install mysql-server -y &>>LOG_FILE
+source ./common.sh
+app_name=mysql
+echo "please enter the root password"
+read -s "MYSQL_ROOT_PASSWORD"
+ dnf install mysql-server -y &>>LOG_FILE
   VALIDATE $? "installing mysql"
   systemctl enable mysqld &>>LOG_FILE
   systemctl start mysqld &>>LOG_FILE 
   VALIDATE $? "starting mysql" 
   mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD &>>LOG_FILE
   VALIDATE $? "setting root password"
+  print_time
--- redis.sh
+++ redis.sh
@@ -1,32 +1,5 @@
-#!/bin/bash
-START_TIME=$(date +%s)
-USER_ID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-mkdir -p $LOGS_FOLDER
-echo "script executed at: $(date)" | tee -a $LOG_FILE
-
-if [ $USER_ID -ne 0 ]
- then
-  echo -e "$R ERROR::please run this script with root  access $N" | tee -a $LOG_FILE
-  exit 1
- else
-  echo -e "$Y you are running this script with root access $N" | tee -a $LOG_FILE
-fi
-  
-  VALIDATE(){
-   if [ $1 -ne 0 ]
-    then
-     echo -e "$2 is...... $R failure $N" | tee -a $LOG_FILE
-   else
-     echo -e "$2 is.... $G success $N" | tee -a $LOG_FILE
-   fi
-  }
+source ./common.sh
+app_name=redis
 
  dnf module disable redis -y &>>$LOG_FILE
  VALIDATE $? "disabling redis"
@@ -47,7 +20,4 @@ fi
  systemctl start redis &>>$LOG_FILE
  VALIDATE $? "starting the redis"
 
- END_TIME=$(date +%s)
- TOTAL_TIME=$(( $END_TIME - $START_TIME))
-
- echo -e "script executed total time=$TOTAL_TIME" | tee -a $LOG_FILE
+ print_time
